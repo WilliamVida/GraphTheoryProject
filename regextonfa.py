3de00ece36f70af615f248a6c39182658b12a2a0
WILLIAM VIDA <williamvidaisgod@gmail.com>
2020-03-18 18:26:41 +0000

    User input

--- regextonfa.py
+++ regextonfa.py
@@ -1,3 +1,18 @@
 import thompsons
 
-print(thompsons.match("a.b|b*", "bbbbbb"))
+
+def menu():
+    print("Enter regular expression")
+    inputRegex = input()
+
+    print("Enter string to compare")
+    inputString = input()
+
+    if thompsons.match(inputRegex, inputString) == True:
+        print("MATCH, the regular expression,", inputRegex,", and the string,", inputString,", entered are a match.")
+
+    else:
+        print("NO MATCH, the regular expression,", inputRegex,", and the string,", inputString,", entered do not match.")
+
+
+menu()
--- thompsons.py
+++ thompsons.py
@@ -188,3 +188,5 @@ if __name__ == "__main__":
     for test in tests:
         assert match(test[0], test[1]) == test[2], test[0] + \
             (" should match " if test[2] else " should not match ")+test[1]
+
+# print(match("a.b|b*", "bbbbbbbbbbb"))
